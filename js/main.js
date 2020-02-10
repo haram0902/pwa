760e7527d1db638cbaea66c802e58d595d138bf9
harrison.kim <harrison.kim@kakaocorp.com>
2020-02-10 15:14:31 +0900

    aaa

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/pwa/sw.js').then(function(){console.log("registered");})
   });
 }
+
+Notification.requestPermission(function(status){
+    console.log(status);
+})
+
+function displayNotification(){
+    if (Notification.permission == 'granted'){
+        navigator.serviceWorker.getRegistration().then(function(reg){
+            reg.showNotification("Hello PUBG!!");
+        })
+    }
+}
+
+const notiButton = document.getElementById('notify');
+
+notiButton.addEventListener('click', displayNotification);
